9c7d3a64e8588420e05841d7a0c0509f9ec5b1e5
hectorProgrammer10 <aguilar.uri1504@gmail.com>
2024-06-23 10:33:50 -0600

    se actualizo para hacer deployd

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-// const corsOptions = {
-//   origin: "*"
-// };
-
 const corsOptions = {
-  origin: ["http://localhost:8081", "http://localhost:5173"],
+  origin: "*",
 };
+
+// const corsOptions = {
+//   origin: ["http://localhost:8081", "http://localhost:5173"],
+// };
 
 app.use(cors(corsOptions));
 
@@ -40,7 +40,7 @@ require("./app/routes/auth.routes")(app);
 require("./app/routes/user.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8181;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
